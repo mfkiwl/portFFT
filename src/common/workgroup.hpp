4ee5cfdb522ea71aa8fe8dfa6d7681e06670ee1e
Finlay Marno <finlay.marno@codeplay.com>
2023-07-11 16:17:10 +0100

    refactor of wg_dft complete

--- src/common/workgroup.hpp
+++ src/common/workgroup.hpp
@@ -34,75 +34,66 @@ namespace sycl_fft {
 //  4. transpose the result
 
 // each sub-fft is done with sg_dft (maybe the columns should be done with wi_dft??)
-//
 
 // do M N-sized dfts, each dft is down a column :'(
-template <direction Dir, int N, int M, int wi_in_sg_per_fft, int fact_wi_N, int SubgroupSize, typename T>
+template <direction Dir, int N, int M, int WIPerFFT, int FFTSizePerWI, int SubgroupSize, typename T>
 __attribute__((always_inline)) inline void column_ffts(sycl::sub_group sg, T* priv, T* loc, T* loc_twiddles) {
-  // wi_in_sg_per_fft = number of work-items in a subgroup working on the FFT
+  constexpr int ffts_per_sg = SubgroupSize / WIPerFFT;
 
-  constexpr int ffts_in_sg = SubgroupSize / wi_in_sg_per_fft;
-  // if things don't divide equally then some of the work items might not be active in the FFT
-  constexpr int active_work_items_in_sg = ffts_in_sg * wi_in_sg_per_fft;
+  // if things don't divide equally then some of the work items might not be active in the FFT, but they still need to
+  // call sg_dft
+  static_assert(M % ffts_per_sg == 0, "need to account for a part of a subgroup breaking from the for-loop");
+  static_assert(SubgroupSize % WIPerFFT == 0, "need to account for some work-items being inactive in the sg dft");
 
+  static_assert(WIPerFFT * FFTSizePerWI == N);
   if constexpr (M == 64 && N == 64) {
-    static_assert(wi_in_sg_per_fft == 32);
-    static_assert(fact_wi_N == 2);
-    static_assert(SubgroupSize == 32);
-    static_assert(ffts_in_sg == 1);
-    static_assert(active_work_items_in_sg == 32);
+    static_assert(WIPerFFT == 32);
+    static_assert(FFTSizePerWI == 2);
+    static_assert(ffts_per_sg == 1);
   }
 
   // which of the sub-groups ffts will this wi do
-  int fft_in_sg = static_cast<int>(sg.get_local_linear_id()) / wi_in_sg_per_fft;
-  int column_begin = static_cast<int>(sg.get_group_id()) * ffts_in_sg + fft_in_sg;
+  int fft_in_sg = static_cast<int>(sg.get_local_linear_id()) / WIPerFFT;
+  const int fft_local_id = static_cast<int>(sg.get_local_linear_id()) % WIPerFFT;
 
-  // stride
-  int column_step = SYCLFFT_SGS_IN_WG * ffts_in_sg;
+  constexpr int column_step = SYCLFFT_SGS_IN_WG * ffts_per_sg;
+  int column_begin = static_cast<int>(sg.get_group_id()) * ffts_per_sg + fft_in_sg;
 
-  // fft_in_sg added so the whole work is always together
-  int column_end = M + fft_in_sg;
-
-  for (int column = column_begin; column < column_end; column += column_step) {
-    bool working = column < M && static_cast<int>(sg.get_local_linear_id()) < active_work_items_in_sg;
-    if (working) {
-      local2private_transposed<fact_wi_N, M, detail::pad::DO_PAD>(
-          loc, priv, static_cast<int>(sg.get_local_linear_id()) % wi_in_sg_per_fft, column);
-    }
-    // do a size N problem
-    sg_dft<Dir, fact_wi_N, wi_in_sg_per_fft>(priv, sg, loc_twiddles + (2 * M));
-    if (working) {
-      private2local_transposed<fact_wi_N, M, detail::pad::DO_PAD>(
-          priv, loc, static_cast<int>(sg.get_local_linear_id()) % wi_in_sg_per_fft, wi_in_sg_per_fft, column);
-    }
+  for (int column = column_begin; column < M; column += column_step) {
+    local2private_transposed<FFTSizePerWI, M, detail::pad::DO_PAD>(loc, priv, fft_local_id, column);
+    sg_dft<Dir, FFTSizePerWI, WIPerFFT>(priv, sg, loc_twiddles + (2 * M));
+    private2local_transposed<FFTSizePerWI, M, detail::pad::DO_PAD>(priv, loc, fft_local_id, WIPerFFT, column);
   }
 }
 
 // do N M-sized dfts, each dft is along a row :D
-template <direction Dir, int N, int M, int fact_sg_M, int fact_wi_M, int SubgroupSize, typename T>
+template <direction Dir, int N, int M, int WIPerFFT, int FFTSizePerWI, int SubgroupSize, typename T>
 __attribute__((always_inline)) inline void row_ffts(sycl::sub_group sg, T* priv, T* loc, T* loc_twiddles,
                                                     const T* wg_twiddles, T scaling_factor) {
-  constexpr int m_ffts_in_sg = SubgroupSize / fact_sg_M;
-  constexpr int max_working_tid_in_sg_m = m_ffts_in_sg * fact_sg_M;
-  int m_sg_offset =
-      static_cast<int>(sg.get_group_id()) * m_ffts_in_sg + static_cast<int>(sg.get_local_linear_id()) / fact_sg_M;
-  int m_sg_increment = SYCLFFT_SGS_IN_WG * m_ffts_in_sg;
-  int max_m_sg_offset = detail::roundUpToMultiple<int>(N, m_ffts_in_sg) +
-                        (static_cast<int>(sg.get_local_linear_id()) >= max_working_tid_in_sg_m);
+  constexpr int ffts_per_sg = SubgroupSize / WIPerFFT;
+  // if things don't divide equally then some of the work items might not be active in the FFT, but they still need to
+  // call sg_dft
+  static_assert(N % ffts_per_sg == 0, "need to account for a part of a subgroup breaking from the for-loop");
+  static_assert(SubgroupSize % WIPerFFT == 0, "need to account for some work-items being inactive in the sg dft");
+  if constexpr (M == 64 && N == 64) {
+    static_assert(WIPerFFT == 32);
+    static_assert(FFTSizePerWI == 2);
+    static_assert(ffts_per_sg == 1);
+  }
 
-  for (int sub_batch = m_sg_offset; sub_batch <= max_m_sg_offset; sub_batch += m_sg_increment) {
-    bool working = sub_batch < N && sg.get_local_linear_id() < max_working_tid_in_sg_m;
-    if (working) {
-      local2private<2 * fact_wi_M, detail::pad::DO_PAD>(
-          loc, priv, sg.get_local_linear_id() % static_cast<std::size_t>(fact_sg_M),
-          static_cast<std::size_t>(2 * fact_wi_M), static_cast<std::size_t>(2 * M * sub_batch));
-    }
+  const int fft_in_sg = static_cast<int>(sg.get_local_linear_id()) / WIPerFFT;
+  const std::size_t fft_local_id = sg.get_local_linear_id() % WIPerFFT;
 
-    // apply twiddles
-    detail::unrolled_loop<0, fact_wi_M, 1>([&](const int i) __attribute__((always_inline)) {
-      int twiddle_n_index = sub_batch;
-      int twiddle_m_index = (static_cast<int>(sg.get_local_linear_id()) % fact_sg_M) * fact_wi_M + i;
-      int twiddle_index = 2 * M * twiddle_n_index + (2 * twiddle_m_index);
+  constexpr int row_step = SYCLFFT_SGS_IN_WG * ffts_per_sg;
+  const int row_begin = static_cast<int>(sg.get_group_id()) * ffts_per_sg + fft_in_sg;
+
+  for (int row = row_begin; row < N; row += row_step) {
+    local2private<2 * FFTSizePerWI, detail::pad::DO_PAD>(loc, priv, fft_local_id, 2 * FFTSizePerWI,
+                                                         static_cast<std::size_t>(2 * M * row));
+
+    detail::unrolled_loop<0, FFTSizePerWI, 1>([&](const int i) __attribute__((always_inline)) {
+      int element = static_cast<int>(fft_local_id) * FFTSizePerWI + i;
+      int twiddle_index = 2 * (M * row + element);
       T twiddle_real = wg_twiddles[twiddle_index];
       T twiddle_imag = wg_twiddles[twiddle_index + 1];
       if constexpr (Dir == direction::BACKWARD) {
@@ -113,18 +104,14 @@ __attribute__((always_inline)) inline void row_ffts(sycl::sub_group sg, T* priv,
       priv[2 * i + 1] = tmp_real * twiddle_imag + priv[2 * i + 1] * twiddle_real;
     });
 
-    // "row" FFTs
-    sg_dft<Dir, fact_wi_M, fact_sg_M>(priv, sg, loc_twiddles);
-    detail::unrolled_loop<0, fact_wi_M, 1>([&](const int i) __attribute__((always_inline)) {
+    sg_dft<Dir, FFTSizePerWI, WIPerFFT>(priv, sg, loc_twiddles);
+    detail::unrolled_loop<0, FFTSizePerWI, 1>([&](const int i) __attribute__((always_inline)) {
       priv[2 * i] *= scaling_factor;
       priv[2 * i + 1] *= scaling_factor;
     });
 
-    if (working) {
-      store_transposed<2 * fact_wi_M, detail::pad::DO_PAD>(
-          priv, loc, sg.get_local_linear_id() % static_cast<std::size_t>(fact_sg_M),
-          static_cast<std::size_t>(fact_sg_M), static_cast<std::size_t>(2 * M * sub_batch));
-    }
+    store_transposed<2 * FFTSizePerWI, detail::pad::DO_PAD>(priv, loc, fft_local_id, WIPerFFT,
+                                                            static_cast<std::size_t>(2 * M * row));
   }
 }
 
