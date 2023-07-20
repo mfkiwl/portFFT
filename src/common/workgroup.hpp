3b3aa79eb6df4e550678dc324626abe863fbcdce
Finlay Marno <finlay.marno@codeplay.com>
2023-07-20 10:28:12 +0100

    vec load wg twiddles

--- src/common/workgroup.hpp
+++ src/common/workgroup.hpp
@@ -93,9 +93,10 @@ __attribute__((always_inline)) inline void row_ffts(sycl::sub_group sg, T* priv,
 
     detail::unrolled_loop<0, FFTSizePerWI, 1>([&](const int i) __attribute__((always_inline)) {
       int element = static_cast<int>(fft_local_id) * FFTSizePerWI + i;
-      int twiddle_index = 2 * (M * row + element);
-      T twiddle_real = wg_twiddles[twiddle_index];
-      T twiddle_imag = wg_twiddles[twiddle_index + 1];
+      int twiddle_index = (M * row + element);
+      sycl::vec<T,2> twiddles = reinterpret_cast<const sycl::vec<T,2>*>(wg_twiddles)[twiddle_index];
+      T twiddle_real = twiddles[0];//wg_twiddles[twiddle_index];
+      T twiddle_imag = twiddles[1];//wg_twiddles[twiddle_index + 1];
       if constexpr (Dir == direction::BACKWARD) {
         twiddle_imag = -twiddle_imag;
       }
